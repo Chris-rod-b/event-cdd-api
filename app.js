a948de84bcb8d1cd66035dc4301e227d30c8b78e
Christian Rodrigues Batista <chris.rod_batista@outlook.com>
2023-07-20 02:49:29 -0300

    feat: resolving CORS Issues - Methods

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const app = express();
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "*")
     next();
 });
 //middleware
